7b58ac2a1aa0c8149ad0bf509e96b0fa74df6883
summercake@qq.com <summercake@qq.com>
2018-05-31 11:36:14 +0000

    ready to config db table fields

---
+++ app/Http/Controllers/ContactPersonController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact_person;
+use Illuminate\Http\Request;
+
+class ContactPersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contact_person  $contact_person
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contact_person $contact_person)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Contact_person  $contact_person
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Contact_person $contact_person)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Contact_person  $contact_person
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contact_person $contact_person)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Contact_person  $contact_person
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contact_person $contact_person)
+    {
+        //
+    }
+}
